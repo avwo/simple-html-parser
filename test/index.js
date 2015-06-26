c53bc34904aa50fd8d1e51ea607465887b0c8a98
avwo <avwu@qq.com>
2015-06-26 09:04:27 +0800

    readme

--- test/index.js
+++ test/index.js
@@ -9,15 +9,17 @@ console.log('=======================================');
 console.log(document.getElementById('test')); 
 
 //通过标签获取元素列表，如果对应的元素不存在，则返回 `[]`
-console.log('--------------通过标签获取元素列表，如果对应的元素不存在，则返回 `[]`----------------');
+console.log('\r\n\r\n\r\n--------------通过标签获取元素列表，如果对应的元素不存在，则返回 `[]`----------------');
 console.log(document.getElementsByTagName('a')); 
+console.log('=======================================');
+console.log(document.getElementsByTagName('input')); 
 console.log('=======================================');
 console.log(document.getElementsByTagName('div')); 
 
 //通过className获取元素列表，如果对应的元素不存在，则返回 `[]`。
 //其中tagName可选，className不支持复合标签
 //如何处理复合标签： 获取对应的elem，再elem = new HTMLParser(elem.innerHTML);
-console.log('--------------通过className获取元素列表，如果对应的元素不存在，则返回 `[]``----------------');
+console.log('\r\n\r\n\r\n--------------通过className获取元素列表，如果对应的元素不存在，则返回 `[]``----------------');
 console.log(document.getElementsByClassName('test3')); 
 console.log('=======================================');
 console.log(document.getElementsByClassName('test2')); 
